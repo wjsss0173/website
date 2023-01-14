54e1d26111363d72fbf194eda78559612cb94802
wjsss0173 <102789640+wjsss0173@users.noreply.github.com>
2023-01-14 22:31:41 +0900

    my website

---
+++ todo.js
@@ -0,0 +1,47 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function cancelLine(event) {
+    
+
+}
+
+function paintToDo(newToDo) {
+    const li = document.createElement("li");
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    const span = document.createElement("span");
+    span.innerText = newToDo;
+    const button = document.createElement("button");
+    button.innerText = "delete";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(checkBox);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li); 
+    console.log(toDoList);
+
+    checkBox.addEventListener("change", () => {
+        if (checkBox.checked === true) {
+          // textDecoration 의 속성을 line-through로 변경
+          span.style.textDecoration = "line-through";
+        } else {
+          span.style.textDecoration = "";
+        }
+      });
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value = ""; 
+    paintToDo(newToDo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
